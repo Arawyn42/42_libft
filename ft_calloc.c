24c74cda94ebf9eee033c44103c6a8e4e6c07903
David Renassia <drenassi@student.42perpignan.fr>
2023-10-16 10:44:58 +0200

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: drenassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 13:41:54 by drenassi          #+#    #+#             */
-/*   Updated: 2023/10/08 16:00:40 by drenassi         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:18:16 by drenassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t n, size_t size)
 {
 	void	*pointer;
 
-	if (size * n < 0 || (size > 0 && n * size / size != n))
+	if (size != 0 && n * size / size != n)
 		return (NULL);
 	pointer = malloc(n * size);
 	if (!pointer)
